6e98554c054d25d24969e5531034cd3ea2ba3441
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-11 12:05:04 -0600

    added new js code for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/country?name=United States',
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
+var YOUR_API_KEY = emiNrDQSin0iMr+zUqMffA==QW7Bq8o94O42es2y
+
+const request = require('request');
+
+request.get({
+  url: 'https://api.api-ninjas.com/v1/country?name=United States',
+  headers: {
+    'X-Api-Key': 'YOUR_API_KEY'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else console.log(body)
 });
