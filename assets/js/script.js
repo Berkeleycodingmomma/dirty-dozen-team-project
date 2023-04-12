ac38c1508ef555811b313d766ae80cbdc8054fd3
The Duz <tamara.dusenbury@gmail.com>
2023-04-12 11:15:18 -0700

    correct fetch for omdb and search button in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,72 @@
 //Test that our APIs work. Will remove once js working
-// var apiKey = "kiXeCA7mju1aj5ELXKhqpFu2BsA3SvXljqdqFaNx"
-// let options = {
-//     method: 'GET',
-//     headers: {
-//         'x-api-key': apiKey
-//     }
-// }
 
-// let url = 'https://api.api-ninjas.com/v1/country?name=united+states'
-// fetch(url, options)
-//     .then(res => res.json()) // parse response as JSON
-//     .then(data => {
-//         console.log(data)
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });  
-// let options1= {
-//     method: 'GET',
-// }
-// let url1 = 'http://www.omdbapi.com/?apikey=b0f2dca4&t=Grease'
-// fetch(url1, options1)
-//     .then(res => res.json()) // parse response as JSON
-//     .then(data => {
-//         console.log(data)
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });
+var input=document.getElementById("movieTitle");
+var apiKey = "kiXeCA7mju1aj5ELXKhqpFu2BsA3SvXljqdqFaNx"
 
- 
-var omdbKey = "b0f2dca4"
-let findTitle=function(input) {
-    let getMovie="https://www.omdbapi.com/?apikey=" + omdbKey + "&t"
-    fetch(getMovie)
-    .then(function(response) {
-        console.log(response);
-        return response.json();
-        })
+$('.btn').on("click", function (event) {
+    event.preventDefault();
+    var title = $("#movieTitle").val();
+   findTitle(title)
+});
+
+
+let options = {
+    method: 'GET',
+    headers: {
+        'x-api-key': apiKey
+    }
 }
 
-$(".btn").on("click", function (event){
-    event.preventDefault();
-    let searchBtn = $(event.target).attr("id")
-    findTitle(searchBtn)
-})
+let findTitle=function(title) {
+console.log(title)
+//let url = 'https://api.api-ninjas.com/v1/country?name=united+states'
+let url = 'https://api.api-ninjas.com/v1/country?name=united+states'
+fetch(url, options)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });  
+let options1= {
+    method: 'GET',
+}
 
+var omdbKey = "b0f2dca4"
+//let url1 = 'http://www.omdbapi.com/?apikey=b0f2dca4&t=Grease'
+let getMovie="http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + title
+console.log(getMovie);
+fetch(getMovie, options1)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+    
+    // .then(function (data) {
+    //     let country = data[0]
+    //     console.log(country)
+    // })
 
+}
 
+// var startPage = function () {
+//     $("#movie-country-facts").hide()
 
-// // var startPage = function () {
-// //     $("#movie-country-facts").hide()
-
-// //     var titleString=JSON.parse(localStorage.getItem("titleString")) || [];
-// //     titleString.forEach(function (title) {
-// //         var appendedButton=$(("<li></li>"))
-// //         appendedButton.text(title)
-// //         appendedButton.attr("id", title)
-// //         appendedButton.addClass("movie btn btn-block")
-// //         var buttonMovieList=$("#movie-title-container")
-// //         buttonMovieList.append(appendedButton)
-// //     })
-// // }
-// // startPage();
+//     var titleString=JSON.parse(localStorage.getItem("titleString")) || [];
+//     titleString.forEach(function (title) {
+//         var appendedButton=$(("<li></li>"))
+//         appendedButton.text(title)
+//         appendedButton.attr("id", title)
+//         appendedButton.addClass("movie btn btn-block")
+//         var buttonMovieList=$("#movie-title-container")
+//         buttonMovieList.append(appendedButton)
+//     })
+// }
+// startPage();
 
 
 // // $("#searchBtn").on("click" || "search", function(event) {
